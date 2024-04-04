2ae498ca537717a4e247b2cddaa59eda59f4adbf
johndeere16 <matteo.tonini@stud.itsaltoadriatico.it>
2024-04-04 17:51:17 +0200

    server-toni

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
-var restify = require('restify');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-var server = restify.createServer();
-server.use(restify.plugins.bodyParser());
+const server = express()
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(express.text()) //mi serve per prendere i dati che mi manda ale 
 
-server.get('/cars', function(req, res, next) {
+server.get('', (req,res,next) =>{
+    return res.send('stutututu!!!');
+})
+
+server.get('/cars', function (req, res, next) {
     res.send('List of cars: [TODO]');
     return next();
 });
 
-server.get('/cars/:id', function(req, res, next) {
+server.get('/cars/:id', function (req, res, next) {
     res.send('Current values for car ' + req.params['id'] + ': [TODO]');
     return next();
 });
 
-server.post('/cars/:id', function(req, res, next) {
+server.post('/cars/:id/metrics', function (req, res, next) {
     res.send('Data received from car [TODO]');
 
     console.log(req.body);
@@ -21,6 +27,6 @@ server.post('/cars/:id', function(req, res, next) {
     return next();
 });
 
-server.listen(8011, function() {
-    console.log('%s listening at %s', server.name, server.url);
+server.listen(8011, function () {
+    console.log('server listening at http://192.168.101.124:8011');
 });
